5518f58eace22dc34b1e46abdff418134e077d31
beapen <github@gulfdoctor.net>
2020-05-28 15:13:53 -0400

    progress with writer

--- src/main/java/in/co/dermatologist/dicoderma/Dicoderma.java
+++ src/main/java/in/co/dermatologist/dicoderma/Dicoderma.java
@@ -7,14 +7,20 @@ import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.Imaging;
 import org.apache.commons.imaging.common.ImageMetadata;
 import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.exif.ExifRewriter;
 import org.apache.commons.imaging.formats.tiff.TiffField;
+import org.apache.commons.imaging.formats.tiff.TiffImageMetadata;
 import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputDirectory;
 
 import com.google.gson.Gson;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.BufferedOutputStream;
 
 @Getter
 @Setter
@@ -46,6 +52,33 @@ public class Dicoderma {
 
     public String getDicodermaMetadataAsString(DicomSCModel dicomSCModel){
         return gson.toJson(dicomSCModel);
+    }
+
+    public byte[] putDicodermaMetadataAsString(BufferedImage bufferedImage, String model){
+        byte[] imageBytes = bufferedImageToByteArray(bufferedImage);
+        final ImageMetadata metadata = Imaging.getMetadata(imageBytes);
+        TiffOutputSet outputSet = new TiffOutputSet();
+
+        // https://stackoverflow.com/questions/10642864/bufferedinputstream-into-byte-to-be-send-over-a-socket-to-a-database
+        ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+        byte[] bytes = bytesOut.toByteArray();
+        BufferedOutputStream os = new BufferedOutputStream(bytesOut);
+
+        //OutputStream os = new BufferedOutputStream(fos);
+        if (metadata instanceof JpegImageMetadata) {
+            final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+            if(null != jpegMetadata){
+                final TiffImageMetadata tiffImageMetadata = jpegMetadata.getExif();
+                if(null != tiffImageMetadata)
+                    // Copy it if present
+                    outputSet = tiffImageMetadata.getOutputSet();
+            }
+            final TiffOutputDirectory exifDirectory = outputSet.getOrCreateExifDirectory();
+            exifDirectory.removeField(ExifTagConstants.EXIF_TAG_USER_COMMENT );
+            exifDirectory.add(ExifTagConstants.EXIF_TAG_USER_COMMENT, model);
+            new ExifRewriter().updateExifMetadataLossless(imageBytes, os, outputSet);
+        }
+        return bytesOut.toByteArray();
     }
 
     private byte[] bufferedImageToByteArray(BufferedImage bufferedImage) {
