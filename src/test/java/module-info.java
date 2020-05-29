c5afa507f3281ac68ce2bfb928ed47a22a96c46b
Bell Eapen <github_public@gulfdoctor.net>
2020-05-29 19:13:36 -0400

    fix: remove module-info.java from java

--- src/test/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module in.co.dermatologist.dicoderma {
-
-    requires org.junit.jupiter.api;
-
-    // requires org.assertj.core;
-    // requires org.mockito;
-    // requires in.co.dermatologist.dicoderma;
-}
