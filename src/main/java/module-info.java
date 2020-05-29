99c95d3460b1a1fe6bcb8456eaa48ceb185b59a8
Bell Eapen <github_public@gulfdoctor.net>
2020-05-29 17:43:40 -0400

    test: Scaffold tests stage 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module in.co.dermatologist.dicoderma {
     requires lombok;
     requires java.desktop;
     requires org.apache.commons.imaging;
-    requires jackson.annotations;
     requires com.google.gson;
     requires dcm4che.tool.common;
     requires dcm4che.core;
@@ -10,4 +9,5 @@ module in.co.dermatologist.dicoderma {
     requires commons.cli;
     requires dcm4che.imageio;
     requires com.fasterxml.jackson.dataformat.javaprop;
+    requires com.fasterxml.jackson.annotation;
 }
